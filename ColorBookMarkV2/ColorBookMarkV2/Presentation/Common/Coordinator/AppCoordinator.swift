1be04032de3bfc9cb530ba25b63eb918ac050318
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2023-06-25 10:45:05 +0900

    Feat: Appcoordinator에 로그인에 따른 플로우 변환 메소드 추가

--- ColorBookMarkV2/ColorBookMarkV2/Presentation/Common/Coordinator/AppCoordinator.swift
+++ ColorBookMarkV2/ColorBookMarkV2/Presentation/Common/Coordinator/AppCoordinator.swift
@@ -11,12 +11,27 @@ import UIKit
 final class AppCoordinator: Coordinator {
     var navigationController: UINavigationController
     var childCoordinators: [Coordinator] = []
+    // 테스트용 플래그.
+    private var isSignUp: Bool = false
     
     init(_ navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
     
     func start() {
+        if isSignUp {
+            startMainFlow()
+        } else {
+            startSignInFlow()
+        }
+    }
+    
+    func startSignInFlow() {
+        let coordinator = SignInCoordinator(navigationController)
+        coordinator.start()
+    }
+    
+    func startMainFlow() {
         let coordinator = MainTabbarCoordinator(navigationController)
         coordinator.start()
     }
