78b1c23f8e99af8d4b2adc4b0ce703dbb5da9cb1
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2023-07-02 12:37:29 +0900

    Feat: 네트워크 레이어 추가

---
+++ ColorBookMarkV2/ColorBookMarkV2/Network/APIConstant.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstant.swift
+//  ColorBookMarkV2
+//
+//  Created by SUN on 2023/07/02.
+//
+
+import Foundation
+
+struct K {
+    static let baseURL: String = Bundle.main.object(forInfoDictionaryKey: "baseURL") as! String
+}
