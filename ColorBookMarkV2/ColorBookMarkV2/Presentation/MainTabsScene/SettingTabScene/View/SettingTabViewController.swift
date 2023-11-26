58c3be6925814aed978f84488e66322fd77e453d
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2023-11-26 11:48:25 +0900

    Feat: 설정 탭 scene 폴더 구성 및 viewcontroller 파일 추가

---
+++ ColorBookMarkV2/ColorBookMarkV2/Presentation/MainTabsScene/SettingTabScene/View/SettingTabViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingTabViewController.swift
+//  ColorBookMarkV2
+//
+//  Created by SUN on 11/26/23.
+//
+
+import Foundation
+import UIKit
+
+final class SettingTabViewController: UIViewController {
+    
+}
